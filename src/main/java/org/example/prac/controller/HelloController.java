2426f98eb286c975dbdbe8a8c9c6ea8f6bfc93cd
gitseoyeon <tjdus553926@gmail.com>
2025-06-26 11:34:13 +0900

    commit: calculator

--- src/main/java/org/example/prac/controller/HelloController.java
+++ src/main/java/org/example/prac/controller/HelloController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
-    @GetMapping("/")
-    public String hello() {
-        return "hello";
-    }
+//    @GetMapping("/")
+//    public String hello() {
+//        return "hello";
+//    }
     @GetMapping("/myPage")
     public String myPage() {
         return "myPage";
